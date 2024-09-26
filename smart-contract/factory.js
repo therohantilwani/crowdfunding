ec092db8d700bccebc5e7405bea837b631f8bc9c
Rohan Jagdish Tilwani <36323861+therohantilwani@users.noreply.github.com>
2024-09-26 14:59:06 -0700

    Update factory.js

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x2Bec4B5E67FE9e6Ba5768D83d49a71A60067B813"
+  "//api"
 );
 
 export default instance;
